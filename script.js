90e9e6565bc4d9cde6990b76142be8a8c475fb71
davlsb <slawotsky@gmail.com>
2024-03-12 14:36:46 -0400

    create variables for current input, last result, operand, and the html buttons

---
+++ script.js
@@ -0,0 +1,8 @@
+//create variables for current input, last result, operand
+
+let currInput = "";
+let lastRes = "";
+let operand = "";
+
+let buttons = document.querySelector("#buttons");
+console.log(buttons);
