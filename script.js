33d38f77c414c047128c35ac5900f2ebab94101a
davlsb <slawotsky@gmail.com>
2024-03-12 15:43:50 -0400

    add backspace/del button functionality

--- script.js
+++ script.js
@@ -21,6 +21,20 @@ buttons.addEventListener("click", (event) => {
             screenInput.innerHTML = '<span class="input-cursor"></span>';
             currInput = "";
         }
+        if (event.target.classList.contains('back')) {
+            console.log(currInput);
+            // Trim currInput to remove leading and trailing whitespace
+            currInput = currInput.trim();
+            // Check if currInput is not empty
+            if (currInput !== "") {
+                // Remove the last character from currInput
+                currInput = currInput.substring(0, currInput.length - 1);
+                screenInput.innerText = currInput;
+            }
+            if (currInput === "") {
+                screenInput.innerHTML = '<span class="input-cursor"></span>';
+            }
+        }
         if(event.target.classList.contains('eq')){
             let newResult = "";
             switch(lastRes[1]){
@@ -38,8 +52,11 @@ buttons.addEventListener("click", (event) => {
                     break;
             } 
             pastCalc.innerText = lastRes[0] + " " + lastRes[1] + " " + currInput;
-            screenInput.innerText = newResult;
-            currInput = newResult;
+            screenInput.innerText = String(newResult);
+            currInput = String(newResult);
+        }
+        if(event.target.classList.contains('github')){
+            window.open("https://github.com/davlsb", '_blank').focus();
         }
     }
 });
